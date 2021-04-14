bbc50c557650c4a04266dd7a73ceb452aea9d14f
cryptoshipwright <cryptoshipwright@protonmail.com>
2021-04-13 17:22:30 -0700

    Add per beneficiary caps to GYFICrowdsale.

--- contracts/GYFICrowdsale.sol
+++ contracts/GYFICrowdsale.sol
@@ -8,22 +8,45 @@ import "./crowdsale/validation/TimedCrowdsale.sol";
 import "./crowdsale/validation/WhitelistCrowdsale.sol";
 
 contract GYFICrowdsale is Crowdsale, AllowanceCrowdsale, CappedCrowdsale, TimedCrowdsale, WhitelistCrowdsale {
+    using SafeMath for uint256;
 
-    constructor(uint256 rate, address payable wallet, IERC20 token, address tokenWallet, uint256 cap, uint openingTime, uint closingTime)
+    uint256 public perBeneficiaryCap;
+
+    mapping(address => uint256) public contribution;
+
+    constructor(
+        uint256 rate,
+        address payable wallet,
+        IERC20 token,
+        address tokenWallet,
+        uint256 cap,
+        uint256 openingTime,
+        uint256 closingTime,
+        uint256 _perBeneficiaryCap
+    )
         Crowdsale(rate, wallet, token)
         AllowanceCrowdsale(tokenWallet)
         CappedCrowdsale(cap)
         TimedCrowdsale(openingTime, closingTime)
         WhitelistAdminRole()
-    {}
+    {
+        perBeneficiaryCap = _perBeneficiaryCap;
+    }
 
     function _deliverTokens(address beneficiary, uint256 tokenAmount) internal override(Crowdsale, AllowanceCrowdsale) {
         AllowanceCrowdsale._deliverTokens(beneficiary, tokenAmount);
     }
     
     function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal override(Crowdsale, CappedCrowdsale, TimedCrowdsale, WhitelistCrowdsale) view {
+        // Per beneficiary cap
+        require(contribution[beneficiary].add(weiAmount) < perBeneficiaryCap, "GYFICrowdsale: Contribution above cap.");
+
         CappedCrowdsale._preValidatePurchase(beneficiary, weiAmount);
         TimedCrowdsale._preValidatePurchase(beneficiary, weiAmount);
         WhitelistCrowdsale._preValidatePurchase(beneficiary, weiAmount);
+    }
+
+    function _updatePurchasingState(address beneficiary, uint256 weiAmount) internal override {
+        contribution[beneficiary] = contribution[beneficiary].add(weiAmount);
     }
 }
