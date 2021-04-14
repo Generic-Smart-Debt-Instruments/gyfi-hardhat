0e143360ff053b4970838dd68d8d41fc05e45416
cryptoshipwright <cryptoshipwright@protonmail.com>
2021-04-13 17:10:23 -0700

    Add GYFICrowdsale

---
+++ contracts/GYFICrowdsale.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "./crowdsale/Crowdsale.sol";
+import "./crowdsale/emission/AllowanceCrowdsale.sol";
+import "./crowdsale/validation/CappedCrowdsale.sol";
+import "./crowdsale/validation/TimedCrowdsale.sol";
+import "./crowdsale/validation/WhitelistCrowdsale.sol";
+
+contract GYFICrowdsale is Crowdsale, AllowanceCrowdsale, CappedCrowdsale, TimedCrowdsale, WhitelistCrowdsale {
+
+    constructor(uint256 rate, address payable wallet, IERC20 token, address tokenWallet, uint256 cap, uint openingTime, uint closingTime)
+        Crowdsale(rate, wallet, token)
+        AllowanceCrowdsale(tokenWallet)
+        CappedCrowdsale(cap)
+        TimedCrowdsale(openingTime, closingTime)
+        WhitelistAdminRole()
+    {}
+
+    function _deliverTokens(address beneficiary, uint256 tokenAmount) internal override(Crowdsale, AllowanceCrowdsale) {
+        AllowanceCrowdsale._deliverTokens(beneficiary, tokenAmount);
+    }
+    
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal override(Crowdsale, CappedCrowdsale, TimedCrowdsale, WhitelistCrowdsale) view {
+        CappedCrowdsale._preValidatePurchase(beneficiary, weiAmount);
+        TimedCrowdsale._preValidatePurchase(beneficiary, weiAmount);
+        WhitelistCrowdsale._preValidatePurchase(beneficiary, weiAmount);
+    }
+}
